db8065aadf5833bb838f97346ae39c8d944f06df
Kristian Kjeldsen <jg@renegade.se>
2023-02-28 14:05:34 +0100

    fixed FK names

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "public"."kkj_project_person" DROP CONSTRAINT "FK_kkj_project_person_project_id";
-ALTER TABLE "public"."kkj_project_person" DROP CONSTRAINT "fkey_person_project";
+ALTER TABLE "public"."kkj_project_person" DROP CONSTRAINT "FK_kkj_person_project_person_id";
 DROP TABLE IF EXISTS "public"."kkj_project";
 DROP TABLE IF EXISTS "public"."kkj_person";
 DROP TABLE IF EXISTS "public"."kkj_project_person";
@@ -21,4 +21,4 @@ CREATE TABLE "public"."kkj_project_person" (
   CONSTRAINT "kkj_project_person_pkey" PRIMARY KEY ("id")
 );
 ALTER TABLE "public"."kkj_project_person" ADD CONSTRAINT "FK_kkj_project_person_project_id" FOREIGN KEY ("project_id") REFERENCES "public"."kkj_project" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
-ALTER TABLE "public"."kkj_project_person" ADD CONSTRAINT "fkey_person_project" FOREIGN KEY ("person_id") REFERENCES "public"."kkj_person" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."kkj_project_person" ADD CONSTRAINT "FK_kkj_person_project_person_id" FOREIGN KEY ("person_id") REFERENCES "public"."kkj_person" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
